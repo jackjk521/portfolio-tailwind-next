f7bebcfff30923aea1ee176942cea7a3a3de434c
jackjk521 <jedchu541@gmail.com>
2023-11-30 16:08:16 +0800

    Updated about page

--- components/about.js
+++ components/about.js
@@ -13,7 +13,7 @@ function About() {
     // <div class="container mx-auto">
     <div class="grid grid-cols-12 gap-4 p-1 mb-3">
       {/* About Me with Image  */}
-      <div class="col-span-12 rounded-lg bg-gray-900 p-5 sm:col-span-8">
+      <div class="col-span-12 rounded-lg flex items-center bg-gray-900 p-5 sm:col-span-8">
         <div class="ps-5 pt-3">
           <Image
             src="/images/profile_img.png"
@@ -23,29 +23,32 @@ function About() {
             alt="profile_img"
           />
         </div>
+        <div class="flex flex-col items-start  px-5 ps-8 pt-4 w-full">
+          <h1 class="font-playfair-display text-xl sm:text-2xl lg:text-4xl font-bold">Full Stack</h1>
+          <h1 class="font-playfair-display text-md sm:text-2xl lg:text-4xl font-bold">Software Engineer</h1>
+          <h1 class="font-playfair-display text-md sm:text-2xl lg:text-4xl font-bold" >with a </h1>
+        </div>
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="w-30 h-20 inline-block"
+          variants={heartVariants}
+          whileHover="hover">
+          <motion.path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+          />
+        </motion.svg>
 
-        <h1 class="font-playfair-display text-xl sm:text-2xl lg:text-4xl font-bold px-5 pt-4"> Full Stack Software Engineer</h1>
-
-        <h1 className="font-playfair-display text-xl sm:text-2xl lg:text-4xl font-bold px-5 flex items-center">
-          with a{" "}
-          <motion.svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="ps-5 w-30 h-20"
-            variants={heartVariants}
-            whileHover="hover">
-            <motion.path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-            />
-          </motion.svg>
-        </h1>
+        {/* <h1 className="font-playfair-display text-xl sm:text-2xl lg:text-4xl font-bold px-5 flex items-center">
+         
+        </h1> */}
       </div>
       {/* <!-- Overview --> */}
       <div class="col-span-12 rounded-lg bg-gray-900 p-5 sm:col-span-4">
-          <SongCard/>
+        <SongCard />
       </div>
     </div>
     // </div>
